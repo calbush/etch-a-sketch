e20b987623877a3a19bcd9ba11fe9a53f8a376f2
Caleb Bush <calebmatthewbush@gmail.com>
2022-07-06 22:23:01 -0500

    Add functionality for changing grid size

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 const container = document.getElementById('container');
-console.log(container)
-/*let div = document.createElement('div')
-container.appendChild(div)
-*/
+
+const btn = document.createElement('button');
+btn.setAttribute('id','btn')
+document.body.prepend(btn)
+console.log(btn)
+btn.textContent = "Click Me!"
+btn.addEventListener('click', setGridSize)
 
 
+function setGridSize () {
+    const squareSize = parseInt(prompt('How many squares per side?'));
 
-for (let i = 0; i < 256; i++) {
-    let div = document.createElement('div')
-    container.appendChild(div)
-    div.classList.add('grid')
-}
-
-const gridSquares = document.querySelectorAll('.grid')
-console.log(gridSquares)
-
-for (square of gridSquares) {
-    square.addEventListener('mouseover', (e) => {
-        e.target.classList.add('black')
+    if (squareSize > 100){
+        alert('Please choose a number smaller than 100')
     }
-    )
+    else if (squareSize <= 100) {
+    const pixelSize = 288 / squareSize;
+    const compensateForBorder = pixelSize - 2;
+    const totalSquares = squareSize ** 2;
+    console.log(totalSquares)
+
+    for (let i = 0; i < totalSquares; i++) {
+        let div = document.createElement('div')
+        container.appendChild(div)
+        div.classList.add('grid')
+    }
+
+    const gridSquares = document.querySelectorAll('.grid')
+
+   
+    for (i of gridSquares){
+        i.style.width = `${compensateForBorder}px`
+        i.style.height = `${compensateForBorder}px`
+        }
+
+    for (square of gridSquares) {
+        square.addEventListener('mouseover', (e) => {
+            e.target.classList.add('black')
+        }
+        )
+        }
+    }
 }
+
+
+
+
+
